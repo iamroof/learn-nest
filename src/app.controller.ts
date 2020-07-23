3743fffdfe4bffe5d78dfe00024a6030b73ad284
Jawahar Rufus <jawahar.rufus1411@gmail.com>
2020-07-23 20:52:36 +0530

    API modified

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('employees')
+  getEmployees(): any {
+    return this.appService.getEmployees();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,25 @@ export class AppService {
   getHello(): string {
     return 'Hello World!'; 
   }
+
+  getEmployees(): any {
+
+    return [
+      {
+        id: 1,
+        name: 'Venkat',
+        gender: 'Male'
+      },
+      {
+        id: 2,
+        name: 'Frank',
+        gender: 'Male'
+      },
+      {
+        id: 3,
+        name: 'jawahar',
+        gender: 'Male'
+      }
+    ]; 
+  }
 }
